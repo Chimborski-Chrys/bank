eb2e71781df480844f5e7a154e65aba0ad352571
Chrystiomar <chrystiomar@hotmail.com>
2021-09-21 00:18:56 -0300

    criando as classes

---
+++ bank/bank/Classes/Conta.cs
@@ -0,0 +1,61 @@
+﻿using bank.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bank.Classes
+{
+    public class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private string Nome { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            TipoConta = tipoConta;
+            Saldo = saldo;
+            Credito = credito;
+            Nome = nome;
+        }
+
+        public bool Sacar(double saque)
+        {
+            if(Saldo - saque < Credito * -1)
+            {
+                Console.WriteLine("Saldo insuficiente");
+                return false;
+            }
+            Saldo -= saque;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", Nome, Saldo);
+            return true;
+        }
+
+        public void Depositar(double deposito)
+        {
+            Saldo += deposito;
+            Console.WriteLine("Saldo atual da conta de {0} é {1}", Nome, Saldo);
+        }
+
+        public void Transferir(double vTransferencia, Conta destino)
+        {
+            if (Sacar(vTransferencia))
+            {
+                destino.Depositar(vTransferencia);
+            }
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Tipo de Conta: " + TipoConta + " | ";
+            retorno += "Nome: " + Nome + " | ";
+            retorno += "Saldo: " + Saldo + " | ";
+            retorno += "Crédito: " + Credito + " | ";
+            return retorno;
+        }
+    }
+}
---
+++ bank/bank/Program.cs
@@ -0,0 +1,107 @@
+﻿using bank.Classes;
+using System;
+using bank.Enum;
+using System.Collections.Generic;
+
+namespace bank
+{
+    class Program
+    {
+        static List<Conta> listContas = new List<Conta>();
+        static void Main(string[] args)
+        {
+            string opcao = MenuUsuario();
+
+            while (opcao.ToUpper() != "X")
+            {
+                switch (opcao)
+                {
+                    case "1":
+                       ListarContas();
+                        break;
+                    case "2":
+                        InserirConta();
+                        break;
+                    case "3":
+                   //     Transferir();
+                        break;
+                    case "4":
+                    //    Sacar();
+                        break;
+                    case "5":
+                     //   Depositar();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                opcao = MenuUsuario();
+            }
+            Console.WriteLine("Obrigado por utilizar nossos serviços.");
+            Console.WriteLine();
+        }
+
+        private static void InserirConta()
+        {
+            Console.WriteLine("Inserir nova Conta: ");
+
+            Console.Write("Digite 1 para Conta Pessoa Fisica ou 2 para Juridica ");
+            int tipoConta = int.Parse(Console.ReadLine());
+
+            Console.Write("Nome do Cliente: ");
+            string nomeCliente = (Console.ReadLine());
+
+            Console.WriteLine("Saldo inicial: ");
+            double saldo = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Informar o crédito: ");
+            double credito = double.Parse(Console.ReadLine());
+
+            Conta novaConta = new Conta(tipoConta: (TipoConta)tipoConta,
+                                        saldo: saldo,
+                                        credito: credito,
+                                        nome: nomeCliente);
+
+            listContas.Add(novaConta);
+        }
+
+        private static void ListarContas()
+        {
+            Console.WriteLine("Lista de Contas"); 
+            if(listContas.Count == 0)
+            {
+                Console.WriteLine("Não existe conta cadastrada.");
+                return;
+            }
+
+            for (int i = 0; i < listContas.Count; i++)
+            {
+                Conta conta = listContas[i];
+                Console.Write("#{0} - ", i);
+                Console.WriteLine(conta);
+            }
+        }
+
+        private static string MenuUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("TDB - The Developer's Bank");
+            Console.WriteLine("Informe a opção: ");
+            Console.WriteLine("1 - Listar Contas");
+            Console.WriteLine("2 - Nova Conta");
+            Console.WriteLine("3 - Transferir Valor");
+            Console.WriteLine("4 - Sacar");
+            Console.WriteLine("5 - Depositar");
+            Console.WriteLine("C - Limpar Tela");
+            Console.WriteLine("X - Sair");
+            Console.WriteLine();
+
+            string opcao = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcao;
+        }
+    }
+}
